f1ed539882d6fc27db8f924fa8b0eaf2e6c0a321
Juan Manuel Pintor <juanmapintor22@gmail.com>
2022-07-27 21:35:35 -0300

    route fix

--- routes/artist.js
+++ routes/artist.js
@@ -15,7 +15,6 @@ api.delete('/artist/:id', md_auth.ensureAuth, ArtistController.deleteArtist);
 api.get('/artists', md_auth.ensureAuth, ArtistController.listArtists);
 api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
 api.post('/upload-image-artist/:id', [md_auth.ensureAuth, md_upload], ArtistController.uploadImage);
-api.post('asdasd', [md_auth.ensureAuth, md_upload], ArtistController.uploadImage);
 api.get('/get-image-artist/:imageFile', ArtistController.getImageFile);
 
 module.exports = api;
