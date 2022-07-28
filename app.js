0e2e563dab0c2e23c7d711ca4a3f30977b456690
Juan Manuel Pintor <juanmapintor22@gmail.com>
2022-07-27 21:29:17 -0300

    ???

--- app.js
+++ app.js
@@ -36,10 +36,10 @@ app.use((req, res, next) => {
 });
 
 //Carga de rutas base.
-app.use('api', user_routes);
-app.use('api', artist_routes);
-app.use('api', album_routes);
-app.use('api', song_routes);
+app.use('/api', user_routes);
+app.use('/api', artist_routes);
+app.use('/api', album_routes);
+app.use('/api', song_routes);
 
 module.exports = app;
 
